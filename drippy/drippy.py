b373f447023e276102a2b521b28cf6f8b421f325
Matthew Karas <1146938+lucidguppy@users.noreply.github.com>
2022-05-30 22:13:29 -0400

    Able to get account info

--- drippy/drippy.py
+++ drippy/drippy.py
@@ -1,16 +1,46 @@
+#!/usr/bin/env python3
 # Copyright 2022 Matthew Karas
 import click
+import requests
 import yaml
+from requests.structures import CaseInsensitiveDict
+from yaml import Loader
+
+BASE_URL = "https://api.digitalocean.com/v2"
 
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    """
+    Set up the context to have a headers item that will help make requests
+    """
+    ctx.ensure_object(dict)
+    with open("config.yaml", "r") as fid:
+        ctx.obj["config"] = yaml.load(fid.read(), Loader=Loader)
+        if "default" in ctx.obj["config"]["auth-contexts"]:
+            token = ctx.obj["config"]["auth-contexts"]["default"]
+            ctx.obj["token"] = token
+            headers = CaseInsensitiveDict()
+            headers["Content-Type"] = "application/json"
+            headers["Authorization"] = f"Bearer {token}"
+            ctx.obj["headers"] = headers
 
 
+@cli.command()
+@click.pass_context
+def account(ctx):
+    url = f"{BASE_URL}/account"
+    print(url)
+    response = requests.get(url, headers=ctx.obj["headers"])
+    print(response.json())
+
+
+@cli.command()
 @click.argument("token")
-@click.option("context", type=click.STRING, default="default")
-def init(token, context):
+@click.option("--context", type=click.STRING, default="default")
+@click.pass_context
+def init(ctx, token, context):
     with open("config.yaml", "w") as fid:
         config = {"auth-contexts": {context: token}}
         yaml.dump(config, fid)
